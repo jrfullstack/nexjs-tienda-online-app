5f070a2e77cd6947a4bdb19a16a7f4e8ab41129d
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-10 19:35:55 +0100

    Productcard hover agregado

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
-import { FC } from 'react';
+import { Box } from '@mui/system';
+import { FC, useMemo, useState } from 'react';
 import { IProduct } from '../../interfaces';
 
 interface Props {
@@ -7,17 +8,36 @@ interface Props {
 }
 
 export const ProductCard: FC<Props> = ({product}) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const productImage = useMemo(() => {
+        return isHovered
+            ? `products/${product.images[0]}`
+            : `products/${product.images[1]}`
+    }, [isHovered, product.images])
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid 
+        item 
+        xs={6} 
+        sm={4}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+    >
         <Card>
             <CardActionArea>
                 <CardMedia
                     component='img'
-                    image={`products/${product.images[0]}`}
+                    className='fadeIn'
+                    image={productImage}
                     alt={product.title}
                 />
             </CardActionArea>
         </Card>
+
+        <Box sx={{mt: 1}} className='fadeIn'>
+            <Typography fontWeight={700}>{product.title}</Typography>
+              <Typography fontWeight={500}>${product.price}</Typography>
+
+        </Box>
     </Grid>
   )
 }
