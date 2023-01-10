ec3f859d53616a6c0035e37d8a23955202a42f95
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-10 23:16:42 +0100

    CartList Creado

---
+++ pages/cart/index.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, Divider, Grid, Typography, Box, Button } from '@mui/material';
+
+import { ShopLayout } from '../../components/layouts/ShopLayout';
+import { CartList } from '../../components/cart/CartList';
+
+
+const CartPage = () => {
+  
+
+  return (
+    <ShopLayout title="Carrito - " pageDescription ="Carrito de compra de la tienda" >
+      <Typography variant='h1' component='h1'>Carrito</Typography>
+
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          {/* CarList */}
+          <CartList/>
+        </Grid>
+
+        <Grid item xs={12} sm={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h2'>Orden</Typography>
+              <Divider sx={{my: 1}}/>
+
+              {/* orden summary */}
+
+              <Box sx={{mt: 3}}>
+                <Button color='secondary' className='circular-btn' fullWidth>
+                  Checkout
+                </Button>
+
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
+
+    </ShopLayout>
+  )
+}
+
+export default CartPage
