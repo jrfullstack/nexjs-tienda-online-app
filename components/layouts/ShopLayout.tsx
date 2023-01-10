a82fd843fc05ba97b60cc4e67ca62c53efe9017a
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-10 07:28:49 +0100

    SideMenu creada

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props {
     title: string;
@@ -31,7 +31,7 @@ export const ShopLayout: FC<Props> = ({children, title, pageDescription, imageFu
               <Navbar/>
         </nav>
 
-          {/* TODO: sidebar */}
+          <SideMenu/>
 
         <main style={{
             margin: '80px auto',
