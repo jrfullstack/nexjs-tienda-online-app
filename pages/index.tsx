d9fe93229076142383239dcc74583c2e2697db60
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-10 07:09:58 +0100

    Home page creada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
+
 
 export default function Home() {
+
   return (
     <ShopLayout title={'Tienda Online - Home'} pageDescription={'Encuentra los mejores productos online aqui'} >
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
