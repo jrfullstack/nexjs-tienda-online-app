77ba3517c81ea9529ab3608ea0ee216e5492473b
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-10 06:55:35 +0100

    Pagina 404 creada

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { ShopLayout } from '../components/layouts';
+
+const Custom404 = () => {
+    return (
+        <ShopLayout title='Page not found' pageDescription='No hay nada que mostrar aqui'>
+            <Box 
+                display='flex' 
+                justifyContent='center' 
+                alignItems='center' 
+                height='calc(100vh - 200px)'
+                sx={{flexDirection: {xs: 'column', sm: 'row'} }}
+            >
+                <Typography variant='h1' component='h1' fontSize={80} fontWeight={200}>404 |</Typography>
+                <Typography marginLeft={2}>Solicitud no encontrada</Typography>
+            </Box>
+        </ShopLayout>
+    )
+}
+
+export default Custom404;
