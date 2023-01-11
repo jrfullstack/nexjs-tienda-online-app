d30478431659b3f638389d60f9be47a3a9d45931
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-11 06:39:32 +0100

    buscar producto por slug

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { db, SHOP_CONSTANTS } from '../../../database';
 import { IProduct } from '../../../interfaces';
 import { Product } from '../../../models';
