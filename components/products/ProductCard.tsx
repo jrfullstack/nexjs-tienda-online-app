d51c90292700caf2d33bf6e6275152fd5a449c94
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-14 00:51:10 +0100

    cargar el texto despues de la imagen

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,48 +1,64 @@
-import { FC, useMemo, useState } from 'react';
+import { FC, useMemo, useState } from "react";
 import NextLink from "next/link";
-import { Card, CardActionArea, CardMedia, Grid, Typography, Box, Link } from '@mui/material';
-import { IProduct } from '../../interfaces';
+import {
+    Card,
+    CardActionArea,
+    CardMedia,
+    Grid,
+    Typography,
+    Box,
+    Link,
+} from "@mui/material";
+import { IProduct } from "../../interfaces";
 
 interface Props {
     product: IProduct;
 }
 
-export const ProductCard: FC<Props> = ({product}) => {
+export const ProductCard: FC<Props> = ({ product }) => {
     const [isHovered, setIsHovered] = useState(false);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
     const productImage = useMemo(() => {
         return isHovered
             ? `products/${product.images[0]}`
-            : `products/${product.images[1]}`
-    }, [isHovered, product.images])
-  return (
-    <Grid 
-        item 
-        xs={6} 
-        sm={4}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-    >
-        <Card>
-            <NextLink href="/product/slug" passHref prefetch={false} legacyBehavior>
-                <Link>
-                    <CardActionArea>
-                        <CardMedia
-                            component='img'
-                            className='fadeIn'
-                            image={productImage}
-                            alt={product.title}
-                        />
-                    </CardActionArea>
-                </Link>  
-            </NextLink>
-            
-        </Card>
+            : `products/${product.images[1]}`;
+    }, [isHovered, product.images]);
+    return (
+        <Grid
+            item
+            xs={6}
+            sm={4}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
+            <Card>
+                <NextLink
+                    href="/product/slug"
+                    passHref
+                    prefetch={false}
+                    legacyBehavior>
+                    <Link>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                className="fadeIn"
+                                image={productImage}
+                                alt={product.title}
+                                // onLoad cuando se cargue ejecuta esto
+                                onLoad={() => setIsImageLoaded(true) }
+                            />
+                        </CardActionArea>
+                    </Link>
+                </NextLink>
+            </Card>
 
-        <Box sx={{mt: 1}} className='fadeIn'>
-            <Typography fontWeight={700}>{product.title}</Typography>
-              <Typography fontWeight={500}>${product.price}</Typography>
-
-        </Box>
-    </Grid>
-  )
-}
+            <Box
+                // Si la imagen esta cargada o no
+                sx={{ mt: 1, display: isImageLoaded ? "block" : "none" }}
+                className="fadeIn">
+                <Typography fontWeight={700}>{product.title}</Typography>
+                <Typography fontWeight={500}>${product.price}</Typography>
+            </Box>
+        </Grid>
+    );
+};
