5b7dcf92bd34620656570dab7f79938118cfc616
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-14 01:21:28 +0100

    activar los en laces en el menu

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,61 +2,84 @@ import NextLink from "next/link";
 import { AppBar, Toolbar, Link, Typography, Box, Button, IconButton } from '@mui/material';
 import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 import Badge from "@mui/material/Badge";
+import { useRouter } from "next/router";
 
 
 export const Navbar = () => {
+
+    const {asPath} = useRouter();
+
   return (
-    <AppBar>
-        <Toolbar>
-            <NextLink href='/' passHref legacyBehavior>
-                <Link display='flex' alignItems='center'>
-                      <Typography variant='h6'>Tienda |</Typography>
-                      <Typography sx={{ml: 0.5}}>Online</Typography>
-                </Link>
-            </NextLink>
+      <AppBar>
+          <Toolbar>
+              <NextLink href="/" passHref legacyBehavior>
+                  <Link display="flex" alignItems="center">
+                      <Typography variant="h6">Tienda |</Typography>
+                      <Typography sx={{ ml: 0.5 }}>Online</Typography>
+                  </Link>
+              </NextLink>
 
-            <Box flex={1} />
+              <Box flex={1} />
 
-            <Box sx={{display: {xs: 'none', sm: 'block'} }}>
-                <NextLink href='/category/men' passHref legacyBehavior>
-                    <Link>
-                        <Button>Hombres</Button>
-                    </Link>
-                </NextLink>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                  <NextLink href="/category/men" passHref legacyBehavior>
+                      <Link>
+                          <Button
+                              color={
+                                  asPath === "/category/men"
+                                      ? "primary"
+                                      : "info"
+                              }>
+                              Hombres
+                          </Button>
+                      </Link>
+                  </NextLink>
 
-                <NextLink href='/category/women' passHref legacyBehavior>
-                    <Link>
-                        <Button>Mujeres</Button>
-                    </Link>
-                </NextLink>
+                  <NextLink href="/category/women" passHref legacyBehavior>
+                      <Link>
+                          <Button
+                              color={
+                                  asPath === "/category/women"
+                                      ? "primary"
+                                      : "info"
+                              }>
+                              Mujeres
+                          </Button>
+                      </Link>
+                  </NextLink>
 
-                <NextLink href='/category/kid' passHref legacyBehavior>
-                    <Link>
-                        <Button>Niños</Button>
-                    </Link>
-                </NextLink>  
-            </Box>     
+                  <NextLink href="/category/kid" passHref legacyBehavior>
+                      <Link>
+                          <Button
+                              color={
+                                  asPath === "/category/kid"
+                                      ? "primary"
+                                      : "info"
+                              }>
+                              Niños
+                          </Button>
+                      </Link>
+                  </NextLink>
+              </Box>
 
-            <Box flex={1} />
+              <Box flex={1} />
 
-            <IconButton>
-                <SearchOutlined/>
-            </IconButton>
+              <IconButton>
+                  <SearchOutlined />
+              </IconButton>
 
-            <NextLink href='/cart' passHref legacyBehavior>
-                <Link>
-                    <IconButton>
-                        <Badge badgeContent={2} color='secondary'>
-                            <ShoppingCartOutlined />
-                        </Badge>
-                    </IconButton>
-                </Link>
-            </NextLink>
+              <NextLink href="/cart" passHref legacyBehavior>
+                  <Link>
+                      <IconButton>
+                          <Badge badgeContent={2} color="secondary">
+                              <ShoppingCartOutlined />
+                          </Badge>
+                      </IconButton>
+                  </Link>
+              </NextLink>
 
-            <Button>
-                Menú
-            </Button>
-        </Toolbar>
-    </AppBar>
-  )
+              <Button>Menú</Button>
+          </Toolbar>
+      </AppBar>
+  );
 }
