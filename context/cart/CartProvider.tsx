962d520db06cadfcf4f3cd1f8a009ee84ddbdf5a
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-16 07:38:32 +0100

    Almacenar carrito en las cookies

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,6 +1,8 @@
-import { FC, PropsWithChildren, useReducer } from 'react';
+import { FC, PropsWithChildren, useEffect, useReducer } from 'react';
+import Cookie from 'js-cookie';
 import { ICartProduct } from '../../interfaces';
 import { CartContext, cartReducer } from './';
+
 
 
 export interface CartState {
@@ -14,6 +16,24 @@ const CART_INITIAL_STATE: CartState = {
 export const CartProvider: FC<PropsWithChildren> = ({children}) => {
 
     const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
+
+    useEffect(() => {
+      // validar la cookie
+      try {
+        const cookieProducts = Cookie.get("cart") ? JSON.parse(Cookie.get('cart')! ): []
+        dispatch({type: '[Cart] - LoadCart from cookies | storage', payload: cookieProducts});        
+      } catch (error) {
+        dispatch({type: '[Cart] - LoadCart from cookies | storage', payload: []}); 
+      }
+        
+    }, []);
+
+    useEffect(() => {
+      Cookie.set('cart', JSON.stringify(state.cart))
+      
+    }, [state.cart])
+    
+
 
     const addProductToCart = (product: ICartProduct) => {
         const productInCart = state.cart.some(p => p._id === product._id);
--- context/cart/cartReducer.ts
+++ context/cart/cartReducer.ts
@@ -13,6 +13,8 @@ export const cartReducer = (state: CartState, action: CartActionType): CartState
     case '[Cart] - LoadCart from cookies | storage':
       return {
         ...state,
+
+        cart: [ ...action.payload ]
       }
     
     case '[Cart] - Update products un cart':
