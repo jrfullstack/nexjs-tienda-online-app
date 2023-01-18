17e90edf9e2249591575aa4f71d354d165d0d127
Jimmy Reyes <jrfullstack@gmail.com>
2023-01-18 10:35:35 +0100

    validacion del token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,4 +1,6 @@
+import { rejects } from "assert";
 import jwt from "jsonwebtoken";
+import { resolve } from "path";
 
 export const signToken = (_id: string, email: string) => {
 
@@ -16,4 +18,22 @@ export const signToken = (_id: string, email: string) => {
         // opciones
         {expiresIn: '30d'}
     )
+}
+
+export const isValidToken = ( token: string ):Promise<string> => {
+    if (!process.env.JWT_SECRET_SEED) {
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+    }
+
+    return new Promise ( (resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) =>  {
+                if(err) return reject('JWT no es valido');
+                const {_id} = payload as {_id: string};
+                resolve(_id);
+            })
+        } catch (error) {
+            reject('JWT no es valido');
+        }
+    })
 }
