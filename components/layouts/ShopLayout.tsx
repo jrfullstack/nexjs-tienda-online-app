6d452da867873b6013cb84254bf9f1d46153f050
Jimmy Reyes <jrfullstack@gmail.com>
2023-03-07 23:07:18 +0100

    footer

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head";
 import { Navbar, SideMenu } from "../ui";
+import { Footer } from "../ui/Footer";
+import { Divider } from "@mui/material";
 
 interface Props {
     title: string;
@@ -11,39 +13,35 @@ interface Props {
 
 export const ShopLayout: FC<Props> = ({children, title, pageDescription, imageFullUrl}) => {
   return (
-    <>
-        <Head>
-            
-            <title>{title}</title>
-            <meta name="description" content={pageDescription} />
+      <>
+          <Head>
+              <title>{title}</title>
+              <meta name="description" content={pageDescription} />
 
-            <meta name="og:title" content={title} />
-            <meta name="og:description" content={pageDescription} />
+              <meta name="og:title" content={title} />
+              <meta name="og:description" content={pageDescription} />
 
-            {
-                imageFullUrl && (
-                    <meta name="og:image" content={imageFullUrl} />
-                )
-            }
-            
-        </Head>
-        <nav>
-              <Navbar/>
-        </nav>
+              {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+          </Head>
+          <nav>
+              <Navbar />
+          </nav>
 
-        <SideMenu/>
+          <SideMenu />
 
-        <main style={{
-            margin: '80px auto',
-            maxWidth: '1440px',
-            padding: '0px 30px'
-        }}>
-            {children}
-        </main>
+          <main
+              style={{
+                  margin: "80px auto",
+                  maxWidth: "1440px",
+                  padding: "0px 30px",
+              }}>
+              {children}
+          </main>
 
-        <footer>
-              {/* TODO: footer */}
-        </footer>
-    </>
-  )
+          <footer>
+              <Divider />
+              <Footer />
+          </footer>
+      </>
+  );
 }
---
+++ components/ui/Footer.tsx
@@ -0,0 +1,95 @@
+import React, { FC, ReactElement } from "react";
+import { Box, Container, Grid, Typography, Divider, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { BabyChangingStationOutlined, Man2Outlined, Woman2Outlined } from "@mui/icons-material";
+import Image from "next/image";
+
+
+export const Footer: FC = (): ReactElement => {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "auto",
+                //   backgroundColor: "primary",
+                paddingTop: "1rem",
+                paddingBottom: "1rem",
+            }}>
+            <Container maxWidth="lg">
+                <Grid
+                    container
+                    direction="row"
+                    // alignItems="center"
+                    justifyContent={"space-between"}>
+                    <Grid item xs={12} sm={4}>
+                        <Typography color="black" variant="h6">
+                            Categorias:
+                        </Typography>
+                        {/* <nav aria-label="secondary mailbox folders"> */}
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemButton
+                                    component="a"
+                                    href="/category/men">
+                                    <Man2Outlined />
+                                    <ListItemText primary="Hombres" />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton
+                                    component="a"
+                                    href="/category/women">
+                                    <Woman2Outlined />
+                                    <ListItemText primary="Mujeres" />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton
+                                    component="a"
+                                    href="/category/kid">
+                                    <BabyChangingStationOutlined />
+                                    <ListItemText primary="Niños" />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                        {/* </nav> */}
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Typography color="black" variant="h6" sx={{ mb: 2 }}>
+                            Contacto:
+                        </Typography>
+                        <Typography>C/ Prta del Sol, 28013</Typography>
+                        <Typography>Madrid, España</Typography>
+                        <Typography sx={{ mt: 2 }}>
+                            Tel: +34 915 22 99 95
+                        </Typography>
+                        <Typography sx={{ mt: 2, mb: 2 }}>
+                            Email: tiendaonline@gmail.com
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Typography color="black" variant="h6" sx={{ mb: 2 }}>
+                            Proximamente!
+                        </Typography>
+                        <Image
+                            src="/store-apple.png"
+                            alt="store"
+                            width={"200"}
+                            height={"115"}
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
+            <Divider />
+            <Grid container justifyContent="center" sx={{ mt: 2 }}>
+                <Grid item>
+                    <Typography color="textSecondary" variant="subtitle2">
+                        {`© ${new Date().getFullYear()} | JrFullstack`}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+// export default Footer;
+// }
